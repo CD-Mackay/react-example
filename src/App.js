d965d187b9bb872b50cd6d9a81bea22fa91fcacc
Connor Mackay <connor.mackay@gmail.com>
2022-03-22 18:05:28 -0400

    build todo component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div>
-        <h2>Title</h2>
-        <button>Delete</button>
-      </div>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,10 @@
+function Todo() {
+  return (
+    <div>
+      <h2>Title</h2>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default Todo;
