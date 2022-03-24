61ca2df39993a309b56ff374271edcff69c87e63
Connor Mackay <connor.mackay@gmail.com>
2022-03-24 19:30:54 -0400

    implement view favourites page

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,6 +1,24 @@
+import { useContext } from 'react';
+
+import { FavouritesContext } from '../store/favourites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function Favourites() {
+  const favouritesCtx = useContext(FavouritesContext);
+
+  let content;
+
+  if (favouritesCtx.totalFavourites === 0) {
+    content = <p>you ain't got no favourites yet!</p>
+  } else {
+    content = <MeetupList meetups={favouritesCtx.favourites} />
+  }
+
   return (
-    <div>I am Favourites</div>
+    <section>
+      <h1>My Favourites</h1>
+      {content}
+    </section>
   )
 };
 
