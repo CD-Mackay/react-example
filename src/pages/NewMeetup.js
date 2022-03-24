4173de8f434f876be7279141c746046d67a92e63
Connor Mackay <connor.mackay@gmail.com>
2022-03-24 16:21:11 -0400

    build/style meetup form

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetups() {
   return (
-    <div>I am NewMeetups</div>
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm  />
+    </section>
   )
 };
 
